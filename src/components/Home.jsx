af357783654d8f137a4583bae761fa911d5a6d2c
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2025-03-04 14:21:51 +0545

    added images to landing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,10 +3,18 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="w-full flex flex-col flex-grow">
-        <div className='p-4 flex flex-row items-center bg-[#d41367]/36'>
-            <h1 class="text-3xl font-bold underline">
-                Hello World!
-            </h1>
+        <div className='p-4 flex flex-row items-center bg-[#d41367]/36 gap'>
+            {/* Left side - image */}
+            <div className="bg-white w-1/2">
+                <img src="src/assets/images/3.-Nepal-Pride-Parade-2019.jpg" alt="homepage-image" className="w-full h-full object-cover"/>
+            </div>
+
+            {/* Right side - text*/}
+            <div className="w-1/2 flex justify-center items-center p-8">
+                <h2 className="text-white text-3xl font-semibold font-amesthysta">
+                    Welcome to <br/> Queer Youth Group Nepal
+                </h2>
+            </div>
         </div>
         <div className="p-4 flex flex-row items-center bg-[#e0e5f5]">
             Something something goes here
