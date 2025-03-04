12f6133cf593f1604d574d6b9a5df9f915963f34
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2025-03-04 16:35:02 +0545

    almost completed landing page; need to age pictures and content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,10 @@ import LandingPageImage from '../assets/images/3.-Nepal-Pride-Parade-2019.jpg'
 const Home = () => {
   return (
     <div className="w-full flex flex-col flex-grow">
+        {/* First container - image and welcome */}
         <div className='p-4 flex flex-col min-[1200px]:flex-row items-center bg-[#d41367]/36 gap-4'>
             {/* Left side - image */}
-            <div className="bg-white w-1/2">
+            <div className="bg-white w-full min-[1200px]:w-1/2 aspect-[16/9] overflow-hidden gap-0">
                 <img src={LandingPageImage} alt="homepage-image" className="w-full h-full object-cover"/>
             </div>
 
@@ -18,15 +19,48 @@ const Home = () => {
                 </h2>
             </div>
         </div>
-        <div className="p-4 flex flex-row items-center bg-[#e0e5f5]">
-            Something something goes here
+
+        {/* Second container - containing books written, etc */}
+        <div className="p-8 flex flex-col md:flex-row justify-between bg-[#f5efe0] gap-8">
+            {/* 4 Books Section */}
+            <div className="md:w-1/3">
+                <h1 className="font-bold font-open-sans text-lg md:text-xl text-center mb-8">4 Books</h1>
+                <ol className="font-roboto list-decimal list-outside pl-5 space-y-2 mb-4">
+                    <li>A Basic Understanding on SOGIESC</li>
+                    <li>Charter of Demands on Legal Recognition of Gender Identity</li>
+                    <li>Ministry of Home Affairs’ directive on ‘other gender’</li>
+                    <li>A Bill regarding Gender Identity, 2021 (Draft)</li>
+                </ol>
+            </div>
+
+            {/* 5 Writ Petitions Section */}
+            <div className="md:w-1/3">
+                <h1 className="font-bold font-open-sans text-lg md:text-xl text-center mb-8">5 Writ Petitions</h1>
+                <p className="font-roboto leading-relaxed mb-4">
+                    5 writ petitions at the Supreme Court of Nepal challenging the law and demanding an 
+                    amendment ensuring rights for queer people.
+                </p>
+            </div>
+
+            {/* 17 Workshops Section */}
+            <div className="md:w-1/3">
+                <h1 className="font-bold font-open-sans text-lg md:text-xl text-center mb-8">17 Workshops</h1>
+                <p className="font-roboto leading-relaxed mb-4">
+                    17 workshops on SOGIESC for young students in partnerships with youth groups.
+                </p>
+            </div>
         </div>
-        <div className="p-4 flex flex-row items-center bg-[#b4ded1]/36">
-            Something something goes here
+
+        {/* Partners */}
+        <div className="p-4 flex justify-center items-center bg-[#b4ded1]/36">
+            <h1 className="font-bold font-open-sans text-lg md:text-xl text-center mb-8"> Our Partners </h1>
         </div>
-        <div className="p-4 flex flex-row items-center bg-[#f5efe0]">
-            Something something goes here
+        
+        {/* Projects and Activities */}
+        <div className="p-4 flex justify-center items-center bg-[#e0e5f5]">
+            <h1 className="font-bold font-open-sans text-lg md:text-xl text-center mb-8"> Our Projects and Activities </h1>
         </div>
+
     </div>
     
   )
